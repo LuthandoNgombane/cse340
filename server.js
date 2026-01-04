4bfbb70d0ad3db945989b91724ddecb14ed3254b
luthandongombane <luthandongombane@gmail.com>
2026-01-04 16:42:57 +0200

    Week 1 Learning Activity

--- server.js
+++ server.js
@@ -6,14 +6,35 @@
  * Require Statements
  *************************/
 const express = require("express")
+
+//LN - include express-ejs-layouts module
+const expressLayouts = require("express-ejs-layouts")
+
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+
+//LN - set EJS as the view engine
+app.set("view engine", "ejs")
+
+//LN - set up express-ejs-layouts middleware
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
